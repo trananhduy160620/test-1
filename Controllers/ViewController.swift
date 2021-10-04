a584962a36c29b42625fbfcc7199aa4d40dd3092
trananhduy <tanhduy160620@gmail.com>
2021-10-05 04:17:31 +0700

    apply mvc architecture

---
+++ Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainProject2
+//
+//  Created by duytran on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Models/Model1.swift
@@ -0,0 +1,8 @@
+//
+//  Model1.swift
+//  MainProject2
+//
+//  Created by duytran on 10/5/21.
+//
+
+import Foundation
